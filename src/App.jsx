7ad4a29bb2c757fc29e25ff97dfb926911ebb4b0
RidhoPK21 <142557165+RidhoPK21@users.noreply.github.com>
2026-03-08 21:32:36 +0700

    logo

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/public/Home";
 import Dashboard from "./pages/admin/Dashboard";
 import AdminLayout from "./layouts/AdminLayout"; // Pastikan path folder-nya sesuai dengan yang Anda buat
-import DataPenduduk from "./pages/admin/DataPenduduk"; 
 import Infografis from "./pages/admin/Infografis"; 
 import KegiatanDesa from "./pages/admin/KegiatanDesa"; 
 import Berita from "./pages/admin/Berita"; 
@@ -29,7 +28,6 @@ export default function App() {
           {/* 'index' berarti ini adalah halaman pertama yang muncul saat membuka /admin */}
           <Route index element={<Dashboard />} />
           {/* Slamet menambahkan baris ini: */}
-          <Route path="penduduk" element={<DataPenduduk />} />
           <Route path="infografis" element={<Infografis />} />
           <Route path="kegiatan" element={<KegiatanDesa />} />
           <Route path="berita" element={<Berita />} />
